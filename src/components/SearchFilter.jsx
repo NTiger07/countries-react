db6c4d8f2d0d6b8cb22eb77ed32b608ca53b6a7b
NTiger07 <falaleru@gmail.com>
2023-01-06 21:10:42 +0100

    search done

--- src/components/SearchFilter.jsx
+++ src/components/SearchFilter.jsx
@@ -1,34 +1,30 @@
-import React from 'react'
-import search from "../assets/search.svg"
+import React from "react";
+import search from "../assets/search.svg";
+import "./SearchFilter.css";
 
 export default function SearchFilter() {
-    return (
-        <>
-            <span className="search_container">
-                <button id="searchBtn">
-                    <img
-                        src={search}
-                        alt="search"
-                        className="svgs"
-                    />
-                </button>
-                <input
-                    type="text"
-                    id="search_input"
-                    placeholder="Search for a country..."
-                />
-            </span>
-            <span className='filter_container'>
-                <span className="label-title">Filter by Region</span>
-                <select name="region" id="filter">
-                    <option value="">All</option>
-                    <option value="&diet=balanced">Africa</option>
-                    <option value="&diet=high-fiber">America</option>
-                    <option value="&diet=high-protein">Asia</option>
-                    <option value="&diet=low-carb">Europe</option>
-                    <option value="&diet=low-fat">Oceania</option>
-                </select>
-            </span>
-        </>
-    )
+  return (
+    <div className="searchfilter_container">
+      <span className="search_container">
+        <button id="searchBtn">
+          <img src={search} alt="search" className="svgs" />
+        </button>
+        <input
+          type="text"
+          id="search_input"
+          placeholder="Search for a country..."
+        />
+      </span>
+      <span className="filter_container">
+        <select name="region" id="filter">
+          <option value="">All</option>
+          <option value="&diet=balanced">Africa</option>
+          <option value="&diet=high-fiber">America</option>
+          <option value="&diet=high-protein">Asia</option>
+          <option value="&diet=low-carb">Europe</option>
+          <option value="&diet=low-fat">Oceania</option>
+        </select>
+      </span>
+    </div>
+  );
 }
