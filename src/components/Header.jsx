7047daf92914335ee7e3912b8f270c8f176edf0b
NTiger07 <falaleru@gmail.com>
2023-01-06 20:14:32 +0100

    heaer styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import moon from "../assets/moon.svg"
+import React from "react";
+import moon from "../assets/moon.svg";
+import "./Header.css";
 
 export default function Header() {
-    return (
-        <div className='header_container'>
-            <h1>Where in the world?</h1>
-            <span><img src={moon} alt="moon" />Dark mode</span>
-        </div>
-    )
+  return (
+    <div className="header_container">
+      <h1>Where in the world?</h1>
+      <span>
+        <img src={moon} alt="moon" className="svgs"/>
+        Dark mode
+      </span>
+    </div>
+  );
 }
