d17b9cd73aed6840f7458423e33f709452f32af9
NTiger07 <falaleru@gmail.com>
2023-01-07 16:28:05 +0100

    onit

--- src/components/CountriesContainer.jsx
+++ src/components/CountriesContainer.jsx
@@ -6,9 +6,11 @@ export default function CountriesContainer() {
   const countries = data.slice(0, 8).map((item) => {
     return (
       <div className="countries_item">
+        <img src="" alt="" />
         <h1>{item.name}</h1>
-        <h3>{item.capital}</h3>
         <span>{item.population}</span>
+        <span>{item.region}</span>
+        <h3>{item.capital}</h3>
       </div>
     );
   });
