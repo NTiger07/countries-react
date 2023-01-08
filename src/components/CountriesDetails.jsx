49c7eea3f5bd3350eeee09eb8813e234f898d1fc
NTiger07 <falaleru@gmail.com>
2023-01-08 22:04:16 +0100

    details start

--- src/components/CountriesDetails.jsx
+++ src/components/CountriesDetails.jsx
@@ -2,5 +2,12 @@ import React from "react";
 import "./CountriesDetails.css";
 
 export default function CountriesDetails() {
-  return <div>CountriesDetails</div>;
+  return (
+    <div className="countries_details-container">
+      <div className="details_image">
+        <img src="" alt="country-flag" />
+      </div>
+      <div className="details_content"></div>
+    </div>
+  );
 }
