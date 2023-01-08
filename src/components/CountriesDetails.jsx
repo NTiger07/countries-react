2ef50253a361d84fc794c9c17d38a22a6b154571
NTiger07 <falaleru@gmail.com>
2023-01-08 22:26:07 +0100

    details skeleton done?

--- src/components/CountriesDetails.jsx
+++ src/components/CountriesDetails.jsx
@@ -1,43 +1,57 @@
 import React from "react";
 import "./CountriesDetails.css";
+import arrow from "../assets/arrow-left.svg";
 
 export default function CountriesDetails() {
   return (
-    <div className="countries_details-container">
-      <div className="details_image">
-        <img src="" alt="country-flag" />
-      </div>
-      <div className="details_content">
-        <h1>Belgium</h1>
-        <div className="content_points">
-          <span>
-            <b>Native Name: </b>Belgie
-          </span>
-          <span>
-            <b>Population: </b>11,319,511
-          </span>
-          <span>
-            <b>Region: </b>Europe
-          </span>
-          <span>
-            <b>Sub Region: </b>Western Europe
-          </span>
-          <span>
-            <b>Capital: </b>Brussels
-          </span>
+    <>
+      <button id="backButton">
+        <span>
+          <img src={arrow} alt="arrow" />
+          Back
+        </span>
+      </button>
+      <div className="countries_details-container">
+        <div className="details_image">
+          <img src="" alt="country-flag" />
         </div>
-        <div className="content_points">
-          <span>
-            <b>Top Level Domain: </b>.be
-          </span>
-          <span>
-            <b>Currencies: </b>Euro
-          </span>
-          <span>
-            <b>Languages: </b>Dutch, French, German
-          </span>
+        <div className="details_content">
+          <h1>Belgium</h1>
+          <div className="content_points">
+            <span>
+              <b>Native Name: </b>Belgie
+            </span>
+            <span>
+              <b>Population: </b>11,319,511
+            </span>
+            <span>
+              <b>Region: </b>Europe
+            </span>
+            <span>
+              <b>Sub Region: </b>Western Europe
+            </span>
+            <span>
+              <b>Capital: </b>Brussels
+            </span>
+          </div>
+          <div className="content_points">
+            <span>
+              <b>Top Level Domain: </b>.be
+            </span>
+            <span>
+              <b>Currencies: </b>Euro
+            </span>
+            <span>
+              <b>Languages: </b>Dutch, French, German
+            </span>
+          </div>
+        </div>
+        <div className="details_border">
+          <span>France</span>
+          <span>Germany</span>
+          <span>Netherlands</span>
         </div>
       </div>
-    </div>
+    </>
   );
 }
