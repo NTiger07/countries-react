4c7dac1f89acc4abb066441306f13a75dbe57e1e
NTiger07 <falaleru@gmail.com>
2023-01-14 00:17:31 +0100

    fix

--- src/components/CountriesContainer.jsx
+++ src/components/CountriesContainer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import CountriesDetails from "./CountriesDetails";
 import data from "./data";
 import "./CountriesContainer.css";
@@ -6,26 +7,28 @@ import "./CountriesContainer.css";
 export default function CountriesContainer() {
   const countries = data.slice(0, 8).map((item) => {
     return (
-      <div className="countries_item">
-        <div className="countries_image">
-          <img src={item.flag} alt="country_image" />
+      <Link to="/details">
+        <div className="countries_item">
+          <div className="countries_image">
+            <img src={item.flag} alt="country_image" />
+          </div>
+          <div className="countries_content">
+            <h1>{item.name}</h1>
+            <span>
+              <b>Population: </b>
+              {item.population}
+            </span>
+            <span>
+              <b>Region: </b>
+              {item.region}
+            </span>
+            <span>
+              <b>Capital: </b>
+              {item.capital}
+            </span>
+          </div>
         </div>
-        <div className="countries_content">
-          <h1>{item.name}</h1>
-          <span>
-            <b>Population: </b>
-            {item.population}
-          </span>
-          <span>
-            <b>Region: </b>
-            {item.region}
-          </span>
-          <span>
-            <b>Capital: </b>
-            {item.capital}
-          </span>
-        </div>
-      </div>
+      </Link>
     );
   });
   return <div className="countries_container">{countries}</div>;
--- src/components/CountriesDetails.jsx
+++ src/components/CountriesDetails.jsx
@@ -1,65 +1,76 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import Header from "./Header";
 import "./CountriesDetails.css";
 import arrow from "../assets/arrow-left.svg";
 
 export default class CountriesDetails extends Component {
   render() {
     return (
-      <div className="countries_details-container">
-        <Link to="/">
-          <button id="backButton">
-            <span>
-              <img src={arrow} alt="arrow" />
-              Back
-            </span>
-          </button>
-        </Link>
-        <div className="details_container">
-          <div className="details_image">
-            <img src="" alt="country-flag" />
-          </div>
-          <div className="details_content">
-            <h1>Belgium</h1>
-            <div className="points-container">
-              <div className="content_pointsone">
-                <span>
-                  <b>Native Name: </b>Belgie
-                </span>
-                <span>
-                  <b>Population: </b>11,319,511
-                </span>
-                <span>
-                  <b>Region: </b>Europe
-                </span>
-                <span>
-                  <b>Sub Region: </b>Western Europe
-                </span>
-                <span>
-                  <b>Capital: </b>Brussels
-                </span>
-              </div>
-              <div className="content_pointstwo">
-                <span>
-                  <b>Top Level Domain: </b>.be
-                </span>
-                <span>
-                  <b>Currencies: </b>Euro
-                </span>
-                <span>
-                  <b>Languages: </b>Dutch, French, German
-                </span>
-              </div>
+      <>
+        {" "}
+        <Header />
+        <div className="countries_details-container">
+          <Link to="/">
+            <button id="backButton">
+              <span>
+                <img src={arrow} alt="arrow" />
+                Back
+              </span>
+            </button>
+          </Link>
+          <div className="details_container">
+            <div className="details_image">
+              <img src={this.props.flag} alt="country-flag" />
             </div>
-            <div className="details_border">
-              <b>Border Countries: </b>
-              <span>France</span>
-              <span>Germany</span>
-              <span>Netherlands</span>
+            <div className="details_content">
+              <h1>Belgium</h1>
+              <div className="points-container">
+                <div className="content_pointsone">
+                  <span>
+                    <b>Native Name: </b>
+                    {this.props.capital}
+                  </span>
+                  <span>
+                    <b>Population: </b>
+                    {this.props.population}
+                  </span>
+                  <span>
+                    <b>Region: </b>
+                    {this.props.region}
+                  </span>
+                  <span>
+                    <b>Sub Region: </b>
+                    {this.props.region}
+                  </span>
+                  <span>
+                    <b>Capital: </b>
+                    {this.props.capital}
+                  </span>
+                </div>
+                <div className="content_pointstwo">
+                  <span>
+                    <b>Top Level Domain: </b>-
+                  </span>
+                  <span>
+                    <b>Currencies: </b>-
+                  </span>
+                  <span>
+                    <b>Languages: </b>
+                    {this.props.languages}
+                  </span>
+                </div>
+              </div>
+              <div className="details_border">
+                <b>Border Countries: </b>
+                <span>-</span>
+                <span>-</span>
+                <span>-</span>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </>
     );
   }
 }
