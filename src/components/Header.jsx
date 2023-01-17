29b6c2a037ef4d6f3ed769b28097b531ffbc4a42
NTiger07 <falaleru@gmail.com>
2023-01-17 19:46:40 +0100

    assed color variables

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,15 @@ import moon from "../assets/moon.svg";
 import "./Header.css";
 
 export default function Header() {
-  const toggle = () => {};
+  const [light, setLight] = React.useState(true);
+  const toggle = () => {
+    
+    if (light === true) {
+      setLight(false);
+    } else {
+      setLight(true);
+    }
+  };
   return (
     <div className="header_container">
       <h1>Where in the world?</h1>
