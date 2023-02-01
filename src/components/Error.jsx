4ad64907eff1f8846082689faca6bfec92b64c6a
NTiger07 <falaleru@gmail.com>
2023-02-01 23:55:51 +0100

    error styling gspeed

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -5,18 +5,19 @@ import arrow from "../assets/arrow-left.svg";
 
 export default function Error() {
   return (
-    <div>
-      <>
-        <Link to="/">
-          <button id="backButton">
-            <span>
-              <img src={arrow} alt="arrow" />
-              Back
-            </span>
-          </button>
-        </Link>
+    <div className="error_container">
+      <Link to="/">
+        <button id="backButton">
+          <span>
+            <img src={arrow} alt="arrow" />
+            Back
+          </span>
+        </button>
+      </Link>
+      <div className="error_content">
         <h1>Error 404</h1>
-      </>
+        <span>Oops, the page you are looking for doesn't exist.</span>
+      </div>
     </div>
   );
 }
